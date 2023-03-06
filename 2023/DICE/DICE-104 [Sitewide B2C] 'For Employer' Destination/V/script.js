fb52f050d9ba91ac9b838ca5bdb020821b47abe7
Harsh34HJ <harsh@funnelenvy.com>
2023-03-06 15:36:42 +0530

    Dice-104 update

--- 2023/DICE/DICE-104 [Sitewide B2C] 'For Employer' Destination/V/script.js
+++ 2023/DICE/DICE-104 [Sitewide B2C] 'For Employer' Destination/V/script.js
@@ -68,32 +68,42 @@
     function loadTest(){
     	document.body.classList.add('dice-104');
     	if(document.querySelector('#navbarDropdown-7')){
-    		FEHelper.updateInnerHtml(document.querySelector('#navbarDropdown-7').parentElement, '<a href="https://www.dice.com/hiring" class="hiring-cta nav-item" data-item-analytics="">For Employers</a>');
+    		if(document.querySelector('#navbarSupportedContent .menu-right').querySelectorAll('.nav-item.dropdown')[1].innerText.trim() == 'Login/Register'){
+    			FEHelper.updateInnerHtml(document.querySelector('#navbarDropdown-7').parentElement, '<a href="https://www.dice.com/hiring" class="hiring-cta nav-item" data-item-analytics="">For Employers</a>');
+    		}
     	}
     	if(document.querySelector('#smart-toggle-for-employer')){
-    		FEHelper.updateInnerHtml(document.querySelector('#smart-toggle-for-employer').parentElement, '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>');
+    		if(document.querySelector('#smart-toggle-for-employer').parentElement.parentElement.querySelectorAll('li')[5].querySelector('a').textContent.trim() == 'Login/Register'){
+    			FEHelper.updateInnerHtml(document.querySelector('#smart-toggle-for-employer').parentElement, '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>');
+    		}
     	}
     	if(document.querySelector('#msmart-toggle-for-employer')){
-    		FEHelper.updateInnerHtml(document.querySelector('#msmart-toggle-for-employer').parentElement, '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>');
+    		if(document.querySelector('#msmart-toggle-for-employer').parentElement.parentElement.querySelectorAll('li')[5].querySelector('a').textContent.trim() == 'Login/Register'){
+    			FEHelper.updateInnerHtml(document.querySelector('#msmart-toggle-for-employer').parentElement, '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>');
+    		}
     	}
-        var check = setInterval(function(){
+		var check = setInterval(function(){
 			if(document.querySelectorAll('dhi-seds-nav-header.hydrated').length > 0 && document.querySelector('dhi-seds-nav-header.hydrated').shadowRoot != null){
 				clearInterval(check);
 				var targetEle = document.querySelector('dhi-seds-nav-header.hydrated').shadowRoot.querySelector('dhi-seds-nav-header-display.hydrated').shadowRoot.querySelectorAll('.links > ul')[1].querySelectorAll('li')[0];
-				targetEle.innerHTML = '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>';
-				targetEle.addEventListener('click', function() {
-		            FEHelper.trackGAEvent("funnelenvy","click","For Employers");
-		        });
+				if(document.querySelector('dhi-seds-nav-header.hydrated').shadowRoot.querySelector('dhi-seds-nav-header-display.hydrated').shadowRoot.querySelectorAll('.links > ul')[1].lastChild.querySelector('.dropdown-button').textContent.trim() == 'Login/Register'){
+					targetEle.innerHTML = '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>';
+					targetEle.addEventListener('click', function() {
+			            FEHelper.trackGAEvent("funnelenvy","click","For Employers");
+			        });
+				}
 			}
 		},500);
-        var check2 = setInterval(function(){
+		var check2 = setInterval(function(){
 			if(document.querySelectorAll('dhi-header.hydrated').length > 0 && document.querySelector('dhi-header.hydrated').shadowRoot != null){
 				clearInterval(check2);
 				var targetEle1 = document.querySelector('dhi-header.hydrated').shadowRoot.querySelectorAll('dhi-list-item.hydrated')[3].shadowRoot.querySelector('.item');
-				targetEle1.innerHTML = '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>';
-				targetEle1.addEventListener('click', function() {
-		            FEHelper.trackGAEvent("funnelenvy","click","For Employers");
-		        });
+				if(document.querySelector('dhi-header.hydrated').shadowRoot.querySelectorAll('dhi-list-item.hydrated')[5].shadowRoot.querySelector('.item').querySelector('.dropdown-button').textContent.trim() == 'Login/Register'){
+					targetEle1.innerHTML = '<a href="https://www.dice.com/hiring" class="hiring-cta nav-link btn-link "target="_self">For Employers</a>';
+					targetEle1.addEventListener('click', function() {
+			            FEHelper.trackGAEvent("funnelenvy","click","For Employers");
+			        });
+				}
 			}
 		},500);
 		FEHelper.live('a.hiring-cta', 'click', function() {
