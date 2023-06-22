8a12c6734184175d27a45f4c832d402ac80ce428
Harsh34HJ <harsh@funnelenvy.com>
2023-06-22 09:44:30 +0530

    Dice-110 Base Structure

---
+++ 2023/DICE/Dice-110 [hiring] Test new contact page designs (built in GO)/V/script.js
@@ -0,0 +1,213 @@
+(function() {
+    var FEHelper = {
+        onLoadElement: function(selector, trigger, delayInterval, delayTimeout) {
+            if (
+                document &&
+                document.querySelectorAll(selector) &&
+                document.querySelectorAll(selector).length > 0
+            ) {
+                trigger();
+            } else {
+                var interval = setInterval(function() {
+                    if (
+                        document &&
+                        document.querySelectorAll(selector) &&
+                        document.querySelectorAll(selector).length > 0
+                    ) {
+                        clearInterval(interval);
+                        trigger();
+                    }
+                }, delayInterval);
+                setTimeout(function() {
+                    clearInterval(interval);
+                }, delayTimeout);
+            }
+        },
+        after: function(targetElement, afterElement) {
+            if (targetElement) {
+                targetElement.insertAdjacentHTML('afterend', afterElement);
+            }
+        },
+        insertAfter: function(afterElement, targetElement) {
+            targetElement.parentNode.insertBefore(
+                afterElement,
+                targetElement.nextSibling
+            );
+        },
+        append: function(targetElement, appendElement) {
+            if (targetElement) {
+                targetElement.insertAdjacentHTML('beforeend', appendElement);
+            }
+        },
+        prepend: function(targetElement, prependElement) {
+            if (targetElement) {
+                targetElement.insertAdjacentHTML('afterbegin', prependElement);
+            }
+        },
+        live: function(selector, event, callback, context) {
+            function addEvent(el, type, handler) {
+                if (el.attachEvent) el.attachEvent('on' + type, handler);
+                else el.addEventListener(type, handler);
+            }
+            this.Element && function(ElementPrototype) {
+                ElementPrototype.matches = ElementPrototype.matches ||
+                    ElementPrototype.matchesSelector ||
+                    ElementPrototype.webkitMatchesSelector ||
+                    ElementPrototype.msMatchesSelector ||
+                    function(selector) {
+                        var node = this,
+                            nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                            i = -1;
+                        while (nodes[++i] && nodes[i] != node);
+                        return !!nodes[i];
+                    };
+            }(Element.prototype);
+            function live(selector, event, callback, context) {
+                addEvent(context || document, event, function(e) {
+                    var found, el = e.target || e.srcElement;
+                    while (el && el.matches && el !== context && !(found = el.matches(selector))) el = el.parentElement;
+                    if (found) callback.call(el, e);
+                });
+            }
+            live(selector, event, callback, context);
+        },
+        trackGAEvent: function($eventCategory, $eventAction, $eventLabel) {
+            if ('ga' in window) {
+                ga.getAll()[0].send('event', {
+                    eventCategory: $eventCategory,
+                    eventAction: $eventAction,
+                    eventLabel: $eventLabel,
+                });
+            }
+        },
+        updateInnerHtml: function(ele, value) {
+            if (ele) {
+                ele.innerHTML = value;
+            }
+        },
+        getScript: function (source, callback) {
+		  var el = document.createElement("script");
+		  el.onload = callback;
+		  el.src = source;
+		  document.body.appendChild(el);
+		}
+    };
+    var imgBaseURL = 'https://fe-test-dev.s3.amazonaws.com/Dice/dice-110';
+
+    var mainTitle = '<div class="info-wrapper"><h1 class="title">Post your jobs where millions of qualified technology professionals are searching.</h1><h4 class="sub-title">Dice is a hiring platform that only does tech. If you’re hiring <strong>engineers, developers, analysts</strong> or <strong>technicians</strong>, you need to be on Dice.</h4></div>';
+    
+    var sideContent = '<div class="content-wrapper"><ul class="list"><li><img src="'+imgBaseURL+'/startup.png" alt="Startup"><div class="text"><div class="bold-text">Fill tech jobs fast</div>The Dice community is expansive, diverse and growing.<ul class="sub-list"><li>Over 5.9M active members</li><li>715K job applications per month</li><li>49K new members per month</li></ul></div></li><li><img src="'+imgBaseURL+'/email-marketing.png" alt="Email Marketing"><div class="text"><div class="bold-text">Access tech talent you can\'t find anywhere else</div>83% of our active tech job seekers aren\'t on Indeed, ZipRecruiter, Monster or CareerBuilder.</div></li><li><img src="'+imgBaseURL+'/user-profile.png" alt="User Profile"><div class="text"><div class="bold-text">More qualified applicants for each job</div>Our apply rate is 201% higher than the industry benchmark.</div></li></ul><div class="logo-wrapper"><p class="title">Thousands of top tech employers hire on Dice, including...</p><picture><source media="(min-width: 768px)" srcset="'+imgBaseURL+'/client_logo_desk_tab.png"><source media="(min-width: 310px)" srcset="'+imgBaseURL+'/client_logo_mob.png"><img src="'+imgBaseURL+'/client_logo_desk_tab.png" alt="Client Logos"></picture></div></div>';
+
+    var titleForm = '<div class="form-title-wrapper"><h4 class="title">Employers & Recruiters</h4><p class="sub-title">Let us show you how we help fill tech jobs faster</p><div class="step-wrap">Step <span class="current-step">1</span> of 2</div></div>';
+
+    var backButton = '<div class="back-wrapper"><a class="back" href="javascript:void(0)"><svg xmlns="http://www.w3.org/2000/svg" width="6" height="12" viewBox="0 0 6 12" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.78033 0.96967C6.07322 1.26256 6.07322 1.73744 5.78033 2.03033L1.81066 6L5.78033 9.96967C6.07322 10.2626 6.07322 10.7374 5.78033 11.0303C5.48744 11.3232 5.01256 11.3232 4.71967 11.0303L0.21967 6.53033C-0.0732233 6.23744 -0.0732233 5.76256 0.21967 5.46967L4.71967 0.96967C5.01256 0.676777 5.48744 0.676777 5.78033 0.96967Z" fill="#195581"/></svg>Back</a></div>';
+        
+    var sectionsBelowHero = '<div class="slider-wrapper"><div class="splide"><div class="splide__arrows"><button class="splide__arrow splide__arrow--prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><rect width="24" height="24" fill="white" fill-opacity="0.01"/><path fill-rule="evenodd" clip-rule="evenodd" d="M14.9949 10.9951C15.1824 11.1826 15.2877 11.4369 15.2877 11.7021C15.2877 11.9672 15.1824 12.2215 14.9949 12.4091L10.4019 17.0021C10.2156 17.1846 9.96482 17.2863 9.70399 17.285C9.44317 17.2837 9.19339 17.1795 9.00896 16.995C8.82452 16.8106 8.72032 16.5608 8.71901 16.3C8.71769 16.0392 8.81936 15.7884 9.00192 15.6021L12.9019 11.7021L9.00192 7.80207C8.81936 7.61578 8.71769 7.36497 8.71901 7.10414C8.72032 6.84331 8.82452 6.59354 9.00896 6.4091C9.19339 6.22467 9.44317 6.12047 9.70399 6.11915C9.96482 6.11784 10.2156 6.21951 10.4019 6.40207L14.9949 10.9951Z" fill="#2B2927"/></svg></button><button class="splide__arrow splide__arrow--next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><rect width="24" height="24" fill="white" fill-opacity="0.01"/><path fill-rule="evenodd" clip-rule="evenodd" d="M14.9949 10.9951C15.1824 11.1826 15.2877 11.4369 15.2877 11.7021C15.2877 11.9672 15.1824 12.2215 14.9949 12.4091L10.4019 17.0021C10.2156 17.1846 9.96482 17.2863 9.70399 17.285C9.44317 17.2837 9.19339 17.1795 9.00896 16.995C8.82452 16.8106 8.72032 16.5608 8.71901 16.3C8.71769 16.0392 8.81936 15.7884 9.00192 15.6021L12.9019 11.7021L9.00192 7.80207C8.81936 7.61578 8.71769 7.36497 8.71901 7.10414C8.72032 6.84331 8.82452 6.59354 9.00896 6.4091C9.19339 6.22467 9.44317 6.12047 9.70399 6.11915C9.96482 6.11784 10.2156 6.21951 10.4019 6.40207L14.9949 10.9951Z" fill="#2B2927"/></svg></button></div><div class="splide__track"><ul class="splide__list"><div class="splide__slide"><img src="'+imgBaseURL+'/heart-icon.svg" alt="Heart Icon" class="auth-img"><div class="auth-info-wrapper"><p class="auth-review">“I like Dice, as I consider it my gold mine of candidates that are not on other sourcing platforms.”</p><h6 class="auth-role">Talent Acquisition Supervisor, DISH</h6></div></div><div class="splide__slide"><img src="'+imgBaseURL+'/heart-icon.svg" alt="Heart Icon" class="auth-img"><div class="auth-info-wrapper"><p class="auth-review">“Being able to search for qualified candidates with the IntelliSearch option was the best part of the platform. Also knowing that candidates are online on the site is key to speed up the communication process. It is easy, very user-friendly and provides great options.”</p><h6 class="auth-role">Talent Acquisition Consultant, Northwestern Mutual</h6></div></div></ul></div></div></div><div class="support-wrapper"><p class="support-text">Need support?</br>Email us: <a href="mailto:support@dice.com">Support@dice.com</a></p></div>';
+    
+    function init() {
+        document.body.classList.add('dice-110');
+        initCSS();
+        if(document.querySelector('.marketo-form-with-media-doc-component')){
+            document.querySelector('.marketo-form-with-media-doc-component').insertAdjacentHTML('beforebegin', mainTitle);
+            document.querySelector('.marketo-form-with-media-doc-component').insertAdjacentHTML('afterend', sectionsBelowHero);
+        }
+        if(document.querySelector('.leadgen-media-simple-doc')){
+            document.querySelector('.leadgen-media-simple-doc').insertAdjacentHTML('beforebegin', sideContent);
+        }
+        onLoadMktoForms2(function(){
+        	MktoForms2.whenReady(function(form){
+		        if(document.querySelector('.marketo-form')){
+		        	document.querySelector('.marketo-form').insertAdjacentHTML('beforebegin', titleForm);
+		        }
+        		updateFormField('.mktoForm .mktoFormRow');
+        		document.body.classList.add('form-load');
+        	});
+        }, 50, 15000);
+    	FEHelper.getScript("https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.1/dist/js/splide.min.js",function () {
+        	if(document.querySelector('.splide')) {
+		        var splide = new Splide(".splide", {
+		            type: 'loop',
+		            perPage: 3,
+		            perMove: 1,
+		            autoplay: true,
+		            autoWidth: true,
+		            autoheight: true,
+		            interval: 6000,
+		            gap: '20px',
+		            focus: 'center',
+		        });
+		        splide.mount();
+		    }
+        });
+        //Back
+        // if (document.querySelector('.step-wrapper-1 .back-wrapper .back')) {
+        // 	FEHelper.live('.step-wrapper-1 .back-wrapper .back', 'click', function() {
+        //         document.querySelector('.step-wrapper-1').classList.add('hide-elm');
+        //         if (document.querySelector('.step-wrapper-0').classList.contains('hide-elm')) {
+        //             document.querySelector('.step-wrapper-0').classList.remove('hide-elm');
+        //         }
+        //         window.scrollTo(0, 0);
+        //     });
+        // }
+        // FEHelper.live('.step-wrapper-0 .option-wrapper .step-option[bm-option="1"] a', 'click', function() {
+        //     FEHelper.trackGAEvent("funnelenvy","click","Create free profile CTA");
+        // });
+        // FEHelper.live('.step-wrapper-0 .option-wrapper .step-option[bm-option="2"] a.primary-cta', 'click', function() {
+        //     FEHelper.trackGAEvent("funnelenvy","click","Post a job CTA");
+        // });
+        // FEHelper.live('.step-wrapper-0 .option-wrapper .step-option[bm-option="2"] a.secondary-cta', 'click', function() {
+        //     FEHelper.trackGAEvent("funnelenvy","click","Source talent CTA");
+        // });
+        // FEHelper.live('.step-wrapper-1 .adscore-banner #people .form-group .form-control', 'click', function() {
+        //     FEHelper.trackGAEvent("funnelenvy","click","Form engagement");
+        // });
+        // FEHelper.live('.step-wrapper-1 .adscore-banner .back-wrapper .back', 'click', function() {
+        //     FEHelper.trackGAEvent("funnelenvy","click","Back button");
+        // });
+        document.body.style.opacity = '1';
+    }
+    //if error occur while DOM manipulation
+    setTimeout(function() {
+        document.body.style.opacity = '1';
+    }, 5000);
+    //add script on body for cache
+    function initCSS() {
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = "https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.1/dist/css/splide.min.css";
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+    function loadTest() {
+        document.body.style.opacity = "0";
+        setTimeout(function() {
+            init();
+        }, 1000);
+    }
+    function onLoadMktoForms2(trigger, delayInterval, delayTimeout){
+        var intervalForMktoForms2 = setInterval(function(){
+            if (typeof window.MktoForms2 != "undefined") {
+                clearInterval(intervalForMktoForms2);
+                trigger();
+            }
+        }, delayInterval);
+        setTimeout(function(){
+            clearInterval(intervalForMktoForms2);
+        }, delayTimeout);
+    }
+    function updateFormField(selector){
+    	document.querySelectorAll(selector).forEach(function(ele){
+            if (ele.querySelector('input, select, textarea')) {
+                var currentName = ele.querySelector('input, select').getAttribute('name');
+                if (currentName == 'FirstName') {currentName = 'First Name';}
+                if (currentName == 'LastName') {currentName = 'Last Name';}
+                if (currentName == 'Email') {currentName = 'Email Address';}
+                if (currentName == 'Phone') {currentName = 'Phone Number';}
+                if (ele.querySelector('label')) {ele.querySelector('label').innerHTML = currentName + ' <div class="mktoAsterix">*</div>';}
+                ele.querySelector('input, select').setAttribute('placeholder', currentName);
+            }
+        });
+    }
+    FEHelper.onLoadElement('body', loadTest, 50, 10000);
+})();
+
